761e88f2a04cad7b082dadf899e2c0618926f17d
Jan Carlo De León <janleoncif11@gmail.com>
2022-08-08 19:51:35 -0600

    Primer commit 1er Parcial DW

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I Parcial Jan Carlo de León</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <!-- Barra de Menu> -->
+    <nav id="menu">
+        <ul>
+            <li> <a href="index.php">Inicio</a></li>
+            <li> <a href="tablas.html">Tablas</a></li>
+            <li> <a href="#">Formularios</a></li>
+        </ul>
+    </nav>
+    <!---------------------------------------------------------------------------->
+
+    <div id="contenedorprincipal">
+
+        <div id="contenedorimagen">
+
+            <img src="imagenhome.jpg" alt="Muestra imagen correspondiente a la página home" width="500">
+    
+        </div>
+        <div id="contenedortexto">
+    
+            <h1>BIENVENIDO</h1>
+    
+            <p>
+                Al examen parcial de Desarrollo Web, en las páginas que usted verá aquí, podrá ver el avance que hemos tenido en las primeras semanas
+                de clase...
+            </p>
+    
+        </div>
+        
+    </div>
+
+    <!--Pie de página-->
+    <footer>
+        <p>
+            Todos los derechos reservados a /UMG/
+        </p>
+    </footer>
+    <!------------------------------------------------------------------------------------------>
+    
+    
+</body>
+</html>
